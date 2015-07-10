a25aebaf6347c654f5cfca894b34b18bfb69858d
Masashi Arino <masashi.arino@gmail.com>
2015-07-10 22:03:43 +0900

    受け取ったお金の判定が出来るようになった。

---
+++ src/VendingBundle/MoneyValidator.php
@@ -0,0 +1,12 @@
+<?php
+namespace VendingBundle;
+
+class MoneyValidator{
+
+    const ACCEPTABLE_MONEY = [10,50,100,500,1000];
+
+    public function checkMoney($money){
+        return in_array($money, self::ACCEPTABLE_MONEY);
+    }
+
+}
--- test/VendingBundle/VendingMachineTest.php
+++ test/VendingBundle/VendingMachineTest.php
@@ -7,6 +7,7 @@
  */
 
 namespace VendingBundleTest;
+use VendingBundle\MoneyValidator;
 use VendingBundle\VendingMachine;
 
 class VendingMachineTest extends \PHPUnit_Framework_TestCase {
@@ -60,5 +61,38 @@ class VendingMachineTest extends \PHPUnit_Framework_TestCase {
      */
     public function お金以外は入らない(){
         $this->vendingMachine->receiveMoney('ぷれいすてーしょんふぉー');
+    }
+
+    /**
+     * @test
+     * @dataProvider actualMoneyList
+     * @param $actualMoney
+     * @param $actualResult
+     */
+    public function 投入可能なお金判定のテスト($actualMoney,$actualResult)
+    {
+        $moneyVaildator = new MoneyValidator();
+        $result = $moneyVaildator->checkMoney($actualMoney);
+        $this->assertEquals($result,$actualResult);
+
+    }
+
+    /**
+     * @return array
+     */
+    public function actualMoneyList()
+    {
+        return [
+            [1,false],
+            [5, false],
+            [10, true],
+            [50, true],
+            [100, true],
+            [500, true],
+            [1000, true],
+            [2000, false],
+            [5000, false],
+            [10000, false]
+        ];
     }
 }
