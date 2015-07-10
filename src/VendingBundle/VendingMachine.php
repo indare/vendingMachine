b69a1121d2774b49aff8bf3c713b520bda23c807
Masashi Arino <masashi.arino@gmail.com>
2015-07-10 10:23:20 +0900

    test can work

---
+++ src/VendingBundle/VendingMachine.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: arino
+ * Date: 15/07/10
+ * Time: 10:05
+ */
+
+namespace VendingBundle;
+
+
+class VendingMachine {
+
+    public function getName()
+    {
+        return 'VendingMachine';
+    }
+}
---
+++ test/VendingBundle/VendingMachineTest.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: arino
+ * Date: 15/07/10
+ * Time: 10:06
+ */
+
+namespace VendingBundleTest;
+use VendingBundle\VendingMachine;
+
+class VendingMachineTest extends \PHPUnit_Framework_TestCase {
+
+    /**
+     * @test
+     */
+    public function GetVendingMachine(){
+        $vendingMachine = new VendingMachine();
+
+        $this->assertEquals('VendingMachine',$vendingMachine->getName());
+    }
+
+
+}
