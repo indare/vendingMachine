3237666090200402b7c5054d097c1264473f8a7b
Masashi Arino <masashi.arino@gmail.com>
2015-07-12 18:53:47 +0900

    Step2の受入試験を追加

---
+++ test/AcceptanceBundle/Step2Test.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: arino
+ * Date: 15/07/12
+ * Time: 18:49
+ */
+
+namespace AcceptanceBundle;
+
+
+use VendingBundle\Entity\Coke;
+use VendingBundle\Machine;
+
+class Step2Test extends \PHPUnit_Framework_TestCase
+{
+    /** @var Machine */
+    private $machine;
+
+    /** @setup */
+    public function setup()
+    {
+        $this->machine = new Machine();
+    }
+
+    /** @test */
+    public function 初期状態で値段120円のコーラを5本格納している()
+    {
+        /** @var array $result */
+        $result = $this->machine->showStock();
+        /** @var Coke $stock */
+        $stock = $result[0];
+
+        $this->assertEquals("コーラ", $stock->getName());
+        $this->assertEquals(120, $stock->getPrice());
+        $this->assertEquals(5, $stock->getCount());
+    }
+}
