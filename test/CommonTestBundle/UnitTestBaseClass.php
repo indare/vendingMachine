1c472ff5c8deee1c70896ebbe788e1a51e1c0140
Masashi Arino <masashi.arino@gmail.com>
2015-07-16 17:46:41 +0900

    DIのタイプヒントを厚くした

--- test/CommonTestBundle/UnitTestBaseClass.php
+++ test/CommonTestBundle/UnitTestBaseClass.php
@@ -3,9 +3,12 @@
 namespace CommonTestBundle;
 
 use Pimple\Container;
+use VendingBundle\Entity\BeverageInterface;
 use VendingBundle\Entity\Coke;
 use VendingBundle\Money\Box\Box;
+use VendingBundle\Money\Box\BoxInterface;
 use VendingBundle\Money\Validator\Validator;
+use VendingBundle\Money\Validator\ValidatorInterface;
 use VendingBundle\Stock;
 
 class UnitTestBaseClass extends \PHPUnit_Framework_TestCase
@@ -17,10 +20,26 @@ class UnitTestBaseClass extends \PHPUnit_Framework_TestCase
     protected function setup()
     {
         $this->container = new Container();
+
+        /** @return int */
         $this->container['beverage.coke.count'] = 5;
+
+        /** @return BoxInterface */
         $this->container['class.box'] = function(){return new Box();};
+
+        /** @return ValidatorInterface */
         $this->container['class.validator'] = function(){return new Validator();};
+
+        /**
+         * @param Container $c
+         * @return BeverageInterface
+         */
         $this->container['class.beverage.coke'] = function($c) {return new Coke($c['beverage.coke.count']);};
+
+        /**
+         * @param Container $c
+         * @return Stock
+         */
         $this->container['class.stock'] = function($c){return new Stock([$c['class.beverage.coke']]);};
     }
 }
