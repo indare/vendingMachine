1e9ccdfaf9a7f08d1abeea422f45bd0308a44aad
Masashi Arino <masashi.arino@gmail.com>
2016-03-04 16:04:28 +0900

    コンテナ設定もいらないー。

--- test/CommonTestBundle/UnitTestBaseClass.php
+++ test/CommonTestBundle/UnitTestBaseClass.php
@@ -3,13 +3,10 @@
 namespace CommonTestBundle;
 
 use Pimple\Container;
-use VendingBundle\Entity\BeverageInterface;
-use VendingBundle\Entity\Coke;
 use VendingBundle\Money\Box\Box;
 use VendingBundle\Money\Box\BoxInterface;
 use VendingBundle\Money\Validator\Validator;
 use VendingBundle\Money\Validator\ValidatorInterface;
-use VendingBundle\Stock;
 
 /**
  * Class UnitTestBaseClass
@@ -30,16 +27,14 @@ class UnitTestBaseClass extends \PHPUnit_Framework_TestCase
         $this->container['beverage.coke.count'] = 5;
 
         /** @return BoxInterface */
-        $this->container['class.box'] = function($c){return new Box();};
+        $this->container['class.box'] = function () {
+            return new Box();
+        };
 
         /** @return ValidatorInterface */
-        $this->container['class.validator'] = function($c){return new Validator();};
-
-        /**
-         * @param Container $c
-         * @return BeverageInterface
-         */
-        $this->container['class.beverage.coke'] = function($c) {return new Coke($c['beverage.coke.count']);};
+        $this->container['class.validator'] = function () {
+            return new Validator();
+        };
 
     }
 
